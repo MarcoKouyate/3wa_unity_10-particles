aeb8164f46b3f1f60a03c32cda7b1f376c4b68ab
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-03-25 16:48:03 +0100

    Zelda Explosion

---
+++ Assets/Explosion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    public class Explosion : MonoBehaviour
+    {
+        [SerializeField] private GameObject _explosionParticle;
+        [SerializeField] private LayerMask _clickableLayers;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Update()
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                Explode();
+            }
+        }
+
+        private void Explode()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, _clickableLayers))
+            {
+                Instantiate(_explosionParticle, hit.point, Quaternion.identity);
+            }
+        }
+    }
+}
