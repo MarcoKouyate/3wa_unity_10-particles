a767ee0d4b21f76cb69eec65aa189d9b56b9add1
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-03-25 11:28:22 +0100

    Recreate particles from "Particles 102"
    https://www.youtube.com/watch?v=rR_bm8f8rVE

---
+++ Assets/Script/Rotator.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+namespace Particles {
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private Transform _child;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        private void Update()
+        {
+            float rotation = _speed * Time.deltaTime;
+            _transform.Rotate(Vector3.forward, rotation);
+            _child.Rotate(Vector3.forward, rotation);
+        }
+
+        private Transform _transform;
+    }
+}
